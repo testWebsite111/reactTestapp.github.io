a2d0e99e5153c178569bc34c668401dede4586f9
testWebpage123 <72172139+testWebpage123@users.noreply.github.com>
2020-10-01 02:52:20 +0300

    second commit

---
+++ precache-manifest.a54c7c3b68702a7d110c3599d9c81318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5a33615f712d06fe281ae91d809f76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f8787b1de0c0d32def",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b2d4b90140c4e3b047b",
+    "url": "/static/js/2.e6c05a14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6c05a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f8787b1de0c0d32def",
+    "url": "/static/js/main.4bf72987.chunk.js"
+  },
+  {
+    "revision": "8cb7ffbf867a0f20890e",
+    "url": "/static/js/runtime-main.a0254fd8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
